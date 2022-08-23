7a988173298c3d2de8f6ab0e20035000bb6d0b7b
laurin steidle <laurin.steidle@uni-hamburg.de>
2022-08-23 12:07:24 +0200

    reintroducing dry_cell stranding; adds stranding experiments

--- experiments/auxiliaries/comp_cost_particle_scaling_analysis.py
+++ experiments/auxiliaries/comp_cost_particle_scaling_analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
 
-path_to_dir = '/scratch/local1/output/22_07_27_comp_cost_particle_scaling_v00'
+path_to_dir = '/scratch/local1/output/22_07_27_comp_cost_particle_scaling_v02'
 cases = load_output_files.get_case_info_files_from_dir(path_to_dir)
 
 df = np.zeros((len(cases),2))
@@ -14,7 +14,7 @@ for ii,item in enumerate(cases):
 
     n_particles = case_info['particle_release_groups'][0]['pulse_size']
     case_run_time = case_info['run_info']['model_run_duration']
-    case_run_time = datetime.strptime(case_run_time,"%H:%M:%S.%f").timestamp()
+    case_run_time = datetime.strptime(case_run_time,"%H:%M:%S.%f").timestamp() - datetime(1900,1,1).timestamp()
 
     df[ii,0] = n_particles
     df[ii,1] = case_run_time
@@ -22,4 +22,4 @@ for ii,item in enumerate(cases):
 print(df)
 
 plt.scatter(df[:,0],df[:,1]-min(df[:,1]))
-plt.savefig('comp_cost_particle_scaling_analysis.png')
+plt.savefig(path_to_dir.split('/')[-1]+'.png')

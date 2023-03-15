e1a6eb99ad18dbba9fdd0a988fe37dc5636af629
laurin steidle <laurin.steidle@uni-hamburg.de>
2023-03-15 01:53:28 +0100

    minor fix; reset path_in_verticle to master

--- oceantracker/post_processing/plotting/plot_vertical_tracks.py
+++ oceantracker/post_processing/plotting/plot_vertical_tracks.py
@@ -29,24 +29,15 @@ def plot_relative_height(tracks_data,  particleID =0, ax = plt.gca(), title='', 
 
 def plot_path_in_vertical_section(tracks_data,  particleID =0,title='', ncase= 0, plot_file_name=None,credit=None):
 
-    threshold = 1
-
     t = tracks_data['time'] / 24. / 3600.
     t = t - t[0]
     ax = plt.gca()
     ax.plot(t, tracks_data['status'][:, particleID], label='Status')
     ax.plot(t, tracks_data['tide'][:, particleID], label='Tide, m', color='k', linewidth=.5)
-    ax.plot(t, tracks_data['tide'][:, particleID]-threshold, label='Tide, m', color='r', linewidth=.5)
+
     ax.plot(t, -tracks_data['water_depth'][:, particleID], label='Water depth, m', color='k')
 
-    z = tracks_data['z'][:,particleID]
-    tide = tracks_data['tide'][:,particleID]
-    above = z[z >= tide-threshold]
-    below = z[z < tide-threshold]
-        
-    ax.plot(t[z >= tide-threshold], above,label='Particle z, m', color = 'g')
-    ax.plot(t[z < tide-threshold], below,label='Particle z, m', color = 'r')
-    # ax.plot(t, tracks_data['z'][:,particleID],label='Particle z, m', color = 'g')
+    ax.plot(t, tracks_data['z'][:,particleID],label='Particle z, m', color = 'g')
 
     ax.set(xlabel='Time, days',ylabel='z, m', title=title)
     ax.legend()

457d341eaeefa6d65f8209fa5e3d28338450e7bb
laurin steidle <laurin.steidle@uni-hamburg.de>
2023-07-19 20:29:42 +0200

    prepared repo for publishing to pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@
 
 from setuptools import setup, find_packages
 
+# Read requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='oceantracker',
       python_requires='>=3.10,<3.11',
-      version='0.4',
+      version='0.4.0',
       description='Fast offline Lagrangian particle tracking in the Ocean',
-      author='',
-      author_email='',
+      long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      author='Ross Vennell',
+      author_email='ross.vennell@cawthron.org.nz',
       url='https://oceantracker.github.io/oceantracker/',
       packages=find_packages(),
-      install_requires=[],
+      install_requires=requirements,
 )

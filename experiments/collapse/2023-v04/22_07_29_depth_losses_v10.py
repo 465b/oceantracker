f6a86af8a6d1fd25ea4b3d86acc3e79ee04193de
laurin steidle <laurin.steidle@uni-hamburg.de>
2023-10-06 16:08:14 +0200

    splitting exp into retention and collapse

--- experiments/collapse/2023-v04/22_07_29_depth_losses_v10.py
+++ experiments/collapse/2023-v04/22_07_29_depth_losses_v10.py
@@ -3,25 +3,18 @@ import os
 import numpy as np
 
 from oceantracker import main
-from oceantracker.post_processing.read_output_files import load_output_files 
-from oceantracker.post_processing.plotting import plot_statistics
 
-#%%
+
+
+
+
 #-----------------------------------------------    
-run_name = '23_08_17_bath_jump_traj_v04'
+run_name = '22_11_01_depth_losses_v10'
 #-----------------------------------------------
 
-"""
-The intention of this experiment is to create a dataset to observe particle
-trajectories around the bathymetric jump in the harbor
-"""
-
-# v04
-# - disabled dispersion
 
 input_dir = "/scratch/local1/hzg2/"
 output_dir = "/scratch/local1/output/"
-# input_dir = "/work/uh0296/u301513/hzg_data/"
 # input_dir = "/work/uh0296/u301513/hzg_data/"
 # output_dir = "/work/uh0296/u301513/ot_output/"
 
@@ -29,20 +22,20 @@ output_dir = "/scratch/local1/output/"
 n_sub_steps = 60*6
 processors = 10
 
-max_time = 3600*24*28*1
+max_time = 3600*24*30*12
 
-max_particle = 1e4
+max_particle = 2e5
 
 pulse_size = 1
-pulse_interval = 10*60 # every minute
+pulse_interval = 60 # every minute
 
 threshold_to_cull = 10
 fraction_to_cull = 1
 
 sa_resolution = 0
-replicates = 1
+replicates = 9
 
-output_step_multiplier = 0.001 # hours between track recorded
+output_step_multiplier = 1 # hours between track recorded
 
 release_polygon = [
     {
@@ -249,94 +242,105 @@ transect_planes = [
 ]
 
 params={
-    'shared_params' :{
-	    "output_file_base": run_name,
-        "root_output_dir": output_dir,
-        "compact_mode": True,
-        "processors": processors,
-        "replicates": replicates,
-    },
+    "root_output_dir": output_dir,
+    "output_file_base": run_name,
+    "debug": False,
+    # "compact_mode": True,
+    # "processors": processors,
+    # "replicates": replicates,
+
+    # "run_params":
+    "write_tracks": True,
+    # "duration": max_time,
+    # "particle_buffer_size": int(max_particle),
+    "open_boundary_type": 0,
+    "block_dry_cells": True,
+
 	"reader": {
-		"class_name": "oceantracker.reader.schism_reader.SCHSIMreaderNCDF",
-        "input_dir": input_dir,
+		"input_dir": input_dir,
 		"file_mask": "schout_*.nc",
+        "class_name": "oceantracker.reader.schism_reader.SCHSIMreaderNCDF",
 		"field_variables": {
 			"water_depth": "depth",
 			"water_salinity": "salt",
             "A_Z": "diffusivity"
 		},
-        'field_variables_to_depth_average': ['water_velocity'],
-		"time_buffer_size": 24,
+        # 'field_variables_to_depth_average': ['water_velocity'],
+		# "time_buffer_size": 24,
 	},
-	"base_case_params": {
-		"run_params": {
-			"write_tracks": True,
-            "duration": max_time,
-			"particle_buffer_size": int(max_particle),
-            "open_boundary_type": 0,
-            "block_dry_cells": True
-		},
-        "solver": {
-            "RK_order": 2,
-            "n_sub_steps": n_sub_steps,
-            "screen_output_step_count": n_sub_steps
-        },
-        "tracks_writer": {
-            "output_step_count": int(24*n_sub_steps*output_step_multiplier)
-        },
-        "dispersion": {
-                'class_name': 'oceantracker.dispersion.random_walk.RandomWalk',
-                'A_V': 0.0,
-                'A_H': 0.0,
+
+    "solver": {
+        "RK_order": 2,
+        "n_sub_steps": n_sub_steps,
+        "screen_output_step_count": int(n_sub_steps*output_step_multiplier)
+    },
+
+    "tracks_writer": {
+        "output_step_count": int(24*n_sub_steps*output_step_multiplier)
+    },
+
+    "dispersion": {
+            'class_name': 'oceantracker.dispersion.random_walk_varyingAz.RandomWalkVaryingAZ',
+            'A_H': 0.1,
+    },
+
+    # "particle_properties": [
+    #     {
+    #         'class_name': 'oceantracker.particle_properties.total_water_depth.TotalWaterDepth'
+    #     }
+    # ], 
+    #     "fields": [
+    #         {
+    #             'class_name': 'oceantracker.fields.friction_velocity.FrictionVelocity'
+    #         },
+    #         {
+    #             'class_name': 'oceantracker.fields.field_vertical_gradient.VerticalGradient',
+    #             'name_of_field': 'A_Z',
+    #             'name': 'A_Z_vertical_gradient'
+    #         }                
+    # ], 
+
+    "resuspension": {
+        "critical_friction_velocity": 0.005
+    },
+
+    "trajectory_modifiers": {
+        "part_culling": {
+            "class_name": "oceantracker.trajectory_modifiers.cull_particles.ParticleCullConcentration",
+            "cull_interval": 60,
+            "threshold_to_cull": threshold_to_cull,
+            "concentration_field": "water_salinity"
+        }
+    },
+
+    "release_groups": {
+        "poly1": {
+            "class_name": "oceantracker.release_groups.polygon_release.PolygonRelease",
+            "points": release_polygon[0]['points'],
+            "pulse_size": int(pulse_size),
+            "release_interval": pulse_interval
+        }
+    },
+
+    "particle_concentrations": {
+        "outfall_conc": {
+            "class_name": "oceantracker.particle_concentrations.particle_concentrations.ParticleConcentrations2D",
+            "case_output_file_tag": "siteA",
+            "update_interval": 1800
+        }
+    },
+
+    'particle_concentrations': {
+            "top_layer": {
+                "class_name": 'oceantracker.particle_concentrations.particle_concentrations.ParticleConcentrationsDepthLayer',
+                "update_interval": 12*3600
             },
-        "particle_properties": [
-            {
-                'class_name': 'oceantracker.particle_properties.total_water_depth.TotalWaterDepth'
+            "full_coloumn": {
+                "class_name": 'oceantracker.particle_concentrations.particle_concentrations.ParticleConcentrations2D',
+                "update_interval": 12*3600
             }
-        ], 
-        "trajectory_modifiers": [
-            {
-                "class_name": "oceantracker.trajectory_modifiers.resuspension.BasicResuspension",
-                "critical_friction_velocity": 0.0
-            },
-            {
-                "class_name": "oceantracker.trajectory_modifiers.cull_particles.ParticleCullConcentration",
-                "cull_interval": 60,
-                "cull_status_greater_than": 'frozen',
-                "threshold_to_cull": threshold_to_cull,
-                "probability_of_culling": fraction_to_cull,
-                "concentration_field": "water_salinity"
-            }
-        ],
-        "fields": [
-                {
-                    'class_name': 'oceantracker.fields.friction_velocity.FrictionVelocity'
-                },
-                {
-                    'class_name': 'oceantracker.fields.field_vertical_gradient.VerticalGradient',
-                    'name_of_field': 'A_Z',
-                    'name': 'A_Z_vertical_gradient'
-                }                
-        ], 
-		"particle_release_groups": [
-			{
-				"class_name": "oceantracker.particle_release_groups.polygon_release.PolygonRelease",
-				"points": release_polygon[0]['points'],
-				"pulse_size": int(pulse_size),
-				"release_interval": pulse_interval
-			}
-        ],
-        "particle_statistics": [
-        ],
-        'particle_concentrations': [
-        ],
-    }
+    },
 }
 
 #%%
 runInfo = main.run(params)
-
-#%%
-path_to_dir = os.path.join(params['shared_params']['root_output_dir'],params['shared_params']['output_file_base'])
-cases = load_output_files.get_case_info_files_from_dir(path_to_dir)
-
